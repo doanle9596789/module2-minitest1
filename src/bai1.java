bec6f6cd78abf68c339b2fea469144772f45d33c
lê đoàn <huongly95967@gmail.com>
2023-01-08 13:43:15 +0700

    songuyentonhohon'n'

---
+++ src/bai1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter n");
+        int n = scanner.nextInt();
+        if (n < 2) {
+            System.out.println("no primes less than 2");
+        } else {
+            int check =2;
+            while (check<n){
+                int sqr=(int)Math.sqrt(check);
+                boolean ok =true;
+                for (int i = 2; i <= sqr; i++) {
+                    if (check % i == 0) {
+                        ok = false;
+                        break;
+                    }
+                }if (ok){
+                    System.out.println(check);
+                }
+                check++;
+            }
+        }
+    }
+}
